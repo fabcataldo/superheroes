2c22745e62a15cf4547bd06e7165663220d2d093
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2025-02-22 18:20:23 -0300

    New test added for uppercase directive.

--- src/app/utils/directives/uppercase.directive.spec.ts
+++ src/app/utils/directives/uppercase.directive.spec.ts
@@ -1,8 +1,27 @@
+import { fakeAsync, tick } from '@angular/core/testing';
 import { UppercaseDirective } from './uppercase.directive';
+import { ElementRef } from '@angular/core';
 
 describe('UppercaseDirective', () => {
+  let directive: UppercaseDirective;
+  let elementInput: ElementRef;
+  let inputElement: HTMLInputElement;
+
+  beforeEach(() => {
+    inputElement = document.createElement('input') as HTMLInputElement;
+    elementInput = new ElementRef(inputElement);
+    directive = new UppercaseDirective(elementInput);
+
+  });
+
   it('should create an instance', () => {
-    const directive = new UppercaseDirective();
     expect(directive).toBeTruthy();
   });
+
+  it('should change input text to uppercase on input event', fakeAsync(() => {
+    inputElement.value = 'testvalue';
+    directive.ngAfterViewInit();
+    tick();
+    expect(inputElement.value).toBe('TESTVALUE');
+  }));
 });
