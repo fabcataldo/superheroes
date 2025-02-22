fd6984700dfc19656be922b7e966ce5625301eb8
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2025-02-22 17:55:21 -0300

    Value of description of some hero modified in ExampleHeroes

--- src/app/utils/testing/consts/ExampleHeroes.ts
+++ src/app/utils/testing/consts/ExampleHeroes.ts
@@ -7,7 +7,7 @@ export const heroes: Hero[] = [
         power: 'Spider-sense',
         years: 35,
         weight: 80,
-        description: 'Lorem ipsum dolor sit amet...'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam urna, tincidunt non velit lacinia, congue fringilla libero. Aenean dignissim consectetur magna non lobortis. In hac habitasse platea dictumst. Sed varius risus diam, at egestas felis gravida at. Suspendisse sodales lorem eu pellentesque interdum. Aenean tristique maximus urna, at vulputate augue ultricies sit amet. Praesent venenatis elit a vehicula ultrices.'
     },
     {
         id: 2,
