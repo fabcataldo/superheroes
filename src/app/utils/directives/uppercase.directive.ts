6ce0bae7465079f1582e8ca65c8a20259f2ddfc3
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2025-02-22 17:00:31 -0300

    New directive added

---
+++ src/app/utils/directives/uppercase.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appUppercase]'
+})
+
+//custom atribute directive type
+export class UppercaseDirective {
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.el.nativeElement.value = this.el.nativeElement.value.toUpperCase();
+    });
+  }
+
+  @HostListener('input', ['$event']) onInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    input.value = input.value.toUpperCase();
+  }
+}
